655b2fb1900e124d78d84c3a712479c2efb815c8
Eric Frey <107942776+ericlfrey@users.noreply.github.com>
2022-10-12 18:03:50 -0500

    Add files via upload

---
+++ playground/index.js
@@ -0,0 +1,8 @@
+const displayText = document.getElementById("console");
+
+//Code whatever you want here
+
+
+// Change these to check your work either in the console or the browser.
+console.log('Hello World!');
+displayText.innerHTML = 'Hello World!';
